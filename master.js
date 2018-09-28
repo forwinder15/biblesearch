9b2b015dc716b7ef5e4fa16cd560706cb2137634
forwinder15 <jrogercoker@gmail.com>
2018-09-27 21:40:08 -0400

    Testing API acesss with fetch

--- master.js
+++ master.js
@@ -1,7 +1,17 @@
-
 const bible = "me Epaphras who is one of you a servant of Christ Jesus greets you always struggling on your behalf in his prayers that you may stand mature and fully assured in all the will of God For I bear him witness that he has worked hard for you and for those in Laodicea and in Hierapolis Luke the beloved physician greets you as does Demas Give my greetings to the brothers at Laodicea and to Nympha and the church in her house And when this letter has been read among you have it also read in the church of the Laodiceans and see that you also read the letter from Laodicea And say to Archippus See that you fulfill the ministry that you have received in the Lord I Paul write this greeting with my own hand Remember my chains Grace be with you";
 const sub = document.querySelector('.sub');
 
+fetch('https://api.esv.org/v3/passage/search/?q=rabble', {
+credentials: 'include',
+header: {
+  'Authorization': 'Token e9db23a61803d3ffcdb9c2caa5a8f3f2b98ca101'
+}})
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(JSON.stringify(myJson));
+  });
 
 function search(e) {
   const word = document.querySelector('.word').value;
